1c92e8a74824396e64ab8da20c644abc4dc49b50
chrysophyt <chrysophyt@gmail.com>
2024-12-08 20:16:51 +0700

    remove -itd flag

--- scripts/init.sh
+++ scripts/init.sh
@@ -17,5 +17,5 @@ REPO=ghcr.io/kaist-ina/stellatrain:main
 docker pull $REPO
 
 # Run
-sudo docker run -itd --rm --gpus all --ipc=host --net=host --ulimit memlock=-1 --ulimit stack=67108864 $REPO \
+sudo docker run --rm --gpus all --ipc=host --net=host --ulimit memlock=-1 --ulimit stack=67108864 $REPO \
   /bin/bash -c "./test_script.sh --master-ip-address $MASTER_IP --my-ip-address $MY_IP --world-size $WORLD_SIZE --num-gpus $NUM_GPUS --rank $RANK"
